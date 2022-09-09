fe590444439794906610661cb24980e7b4742b64
Dummk0pf <110558786+Dummk0pf@users.noreply.github.com>
2022-09-09 22:40:39 +0530

    broken academy section

--- edu.js
+++ edu.js
@@ -1,9 +1,17 @@
-function navdisplay(){
-    var state = document.getElementById("subdis");
+function navdisplay(ref){
+    var id = ref.id;
+    var state = document.getElementById(id);
+    console.log('element id:' + id);
+    state.style.display = "none";
     if(state.style.display === "none"){
         state.style.display = "block";
     }
-    else{
+}
+
+function navexit(ref){
+    var id = ref.id;
+    var state = document.getElementById(id);
+    if(state.style.display === "block"){
         state.style.display = "none";
     }
 }
