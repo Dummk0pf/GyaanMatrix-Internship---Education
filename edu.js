9fff5d6734a5f3e247a5b1f42b43ee0232987b8a
Dummk0pf <110558786+Dummk0pf@users.noreply.github.com>
2022-09-09 07:18:31 +0530

    changed nav

---
+++ edu.js
@@ -0,0 +1,9 @@
+function navdisplay(){
+    var state = document.getElementById("subdis");
+    if(state.style.display === "none"){
+        state.style.display = "block";
+    }
+    else{
+        state.style.display = "none";
+    }
+}
